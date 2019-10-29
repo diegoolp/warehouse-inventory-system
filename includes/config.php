0ac539add10683487b7243d74c9ea9045ddc9e26
diegoolp <33547888+diegoolp@users.noreply.github.com>
2019-10-29 11:43:05 -0600

    Cambios en conexión de base de datos

--- includes/config.php
+++ includes/config.php
@@ -13,8 +13,8 @@
 |
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_USER', 'azure' );             // Set database user
+  define( 'DB_PASS', '6#vWHD_$' );             // Set database password
+  define( 'DB_NAME', 'localdb' );        // Set database name
 
 ?>
