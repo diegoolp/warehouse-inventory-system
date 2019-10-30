56d4dd86b762d2267db7f61b4fab2d63211e2611
diegoolp <33547888+diegoolp@users.noreply.github.com>
2019-10-29 21:28:42 -0600

    Prueba Pruebas selenium

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <form method="post" action="auth.php" class="clearfix">
         <div class="form-group">
               <label for="username" class="control-label">Usario</label>
-              <input type="name" class="form-control" name="username" placeholder="Usario">
+              <input type="name" class="form-control" name="username" placeholder="Usuario">
         </div>
         <div class="form-group">
             <label for="Password" class="control-label">Contraseña</label>
