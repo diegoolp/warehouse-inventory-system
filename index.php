455c2997a1c9371162b3f531f3b9fe4da0360294
diegoolp <33547888+diegoolp@users.noreply.github.com>
2019-10-29 21:50:24 -0600

    Prueba MS TEST

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
       <form method="post" action="auth.php" class="clearfix">
         <div class="form-group">
               <label for="username" class="control-label">Usario</label>
-              <input type="name" class="form-control" name="username" placeholder="Usuario">
+              <input type="name" class="form-control" name="username" placeholder="Usario">
         </div>
         <div class="form-group">
             <label for="Password" class="control-label">Contraseña</label>
